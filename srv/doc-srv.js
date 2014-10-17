a54c2c8ff4a5e1160a990640e9df70a33c85a9df
Peter Elger <elger.peter@gmail.com>
2014-10-17 17:21:25 +0100

    first commit

---
+++ srv/doc-srv.js
@@ -0,0 +1,10 @@
+//var redis_host = process.env.REDIS_HOST||'127.0.0.1'
+//var beanstalk_host = process.env.BEANSTALK_HOST||'127.0.0.1'
+
+require('seneca')()
+//  .use('jsonfile-store',{folder:'../data/doc'})
+  .use('../lib/doc.js')
+  //.listen({type:'queue',pin:{role:'doc',kind:'clock',cmd:'*'},host:beanstalk_host})
+  //.client({type:'pubsub',host:redis_host})
+  .listen({host: '0.0.0.0', port:9002})
+
